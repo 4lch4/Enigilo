3fc254be501c34d06ec2629ea3ef47c2e192351b
Devin Leaman <dleaman212@gmail.com>
2018-08-15 22:26:00 -0500

    InsertLink functional
    The InsertLink command is now functional, and is rather stable. I'd like to build an actual extension and publish it for further testing.

--- extension.js
+++ extension.js
@@ -2,35 +2,79 @@ const vscode = require('vscode')
 const Window = vscode.window
 const Range = vscode.Range
 
+const { showMessage } = require('./tools/StdTools')
+const {
+  parseExistingLinks,
+  getMaxIndex,
+  getLinkUrl
+} = require('./tools/LinkTools')
+const Selection = require('vscode').window.activeTextEditor.selection
+
+/**
+ * Verifies the provided selection isn't a blank selection.
+ *
+ * @param {Selection} selection
+ *
+ * @returns {boolean} True or false, is the selection a valid selection?
+ */
+const verifySelection = selection => {
+  if (selection.start.isEqual(selection.end)) {
+    showMessage(Window, 'Please make a selection before executing this command.')
+      .catch(err => Window.showErrorMessage(err.message))
+    return false
+  } else return true
+}
+
+/**
+ * The function/command for inserting a link into a Markdown file.
+ */
 const insertLink = () => {
   let editor = Window.activeTextEditor
-  let selections = editor.selections
+  let selection = editor.selection
   let doc = editor.document
 
-  // Iterate over each selection in the collection
-  selections.forEach(selection => {
-    // Perform an edit on each selection to make it a link
-    editor.edit(edit => {
-      // The OG text to begin with
-      let ogText = doc.getText(new Range(selection.start, selection.end))
+  getLinkUrl(Window).then(url => {
+    if (url.length === 0) return
 
-      // The New School text to replace it with
-      let nsText = `[${ogText}][0]`
+    let existingLinks = parseExistingLinks(doc)
+    let maxIndex = getMaxIndex(existingLinks)
 
-      // Perform the actual replacement
-      edit.replace(selection, nsText)
+    if (verifySelection(selection)) {
+      try {
+        let link = {
+          index: maxIndex,
+          url: url,
+          lineNum: doc.lineCount
+        }
 
-      // Let the user know of the change
-      Window.showInformationMessage(`${ogText} has been replaced with ${nsText}`)
-    })
-  })
+        editor.edit(builder => {
+        // The OG text to begin with
+          let ogText = doc.getText(new Range(selection.start, selection.end))
+
+          // The New School text to replace it with
+          let nsText = `[${ogText}][${link.index}]`
+
+          // Perform the actual replacement
+          builder.replace(selection, nsText)
+
+          // The position for the actual url to go
+          let position = new vscode.Position(link.lineNum, 0)
+
+          // The formatted string containing the index and url
+          let content = `\n[${link.index}]: ${link.url}`
+
+          // Insert the url reference at the bottom of the file
+          builder.insert(position, content)
+        })
+      } catch (err) { Window.showErrorMessage(err.message) }
+    }
+  }).catch(err => Window.showErrorMessage(err.message))
 }
 
 exports.activate = () => {
-  console.log(`Link-Inserter has been activated.`)
+  // console.log(`Enmeti has been activated.`)
+
   vscode.commands.registerCommand('extension.insertLink', insertLink)
 }
 
-exports.deactivate = () => {
-
-}
+exports.deactivate = () => {}
---
+++ tools/LinkTools.js
@@ -0,0 +1,103 @@
+/** A regex statement to test if the string starts with a link reference. */
+const urlStarterRegex = /\[\d+\]: /
+
+/**
+ * Parses the provided document for any existing links by testing each line,
+ * starting from the last line and moving up, to see if it matches the
+ * "\[\d*\]: " regex statement. If so, it's added to the links collection.
+ *
+ * @param {Doc} doc
+ *
+ * @returns {Links} A collection of links found in the document
+ */
+const parseExistingLinks = doc => {
+  let links = []
+
+  try {
+    for (let x = doc.lineCount - 1; x >= 0; x--) {
+      let lineText = doc.lineAt(x).text
+
+      if (urlStarterRegex.test(lineText)) {
+        links.push({
+          index: getLinkIndex(lineText),
+          url: getLinkUrl(lineText),
+          lineNum: x
+        })
+      }
+    }
+  } catch (err) {
+    console.error(`An error has occured while trying to parse existing links: ${err.message}`)
+    return []
+  }
+
+  return links
+}
+
+module.exports.parseExistingLinks = parseExistingLinks
+
+/**
+ * Gets the url of the link referenced in the provided string.
+ *
+ * @param {string} text The line containing the link
+ */
+const getLinkUrl = text => {
+  let start = text.indexOf(']: ') + 3
+
+  return text.substring(start)
+}
+
+/**
+ * Gets the link index, or number between the brackets, of the provided string.
+ *
+ * @param {string} text The line containing the link
+ */
+const getLinkIndex = text => {
+  let start = text.indexOf('[') + 1
+  let end = text.indexOf(']')
+
+  return text.substring(start, end)
+}
+
+/**
+ *
+ * @param {Links[]} links
+ *
+ * @returns {number}
+ */
+const getMaxIndex = links => {
+  let maxIndex = -1
+
+  links.forEach(link => {
+    if (link.index > maxIndex) maxIndex = link.index
+  })
+
+  maxIndex++
+
+  return maxIndex
+}
+
+module.exports.getMaxIndex = getMaxIndex
+
+const getLinkUrlFromUser = window => {
+  let urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,15}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/
+
+  return new Promise((resolve, reject) => {
+    window.showInputBox({
+      placeHolder: 'https://hasslefree.solutions',
+      prompt: 'What is the URL this link should point to?',
+      validateInput: val => {
+        if (urlRegex.test(val)) return null
+        else return 'Please provide a value url.'
+      }
+    }).then(res => resolve(res), err => { if (err) reject(err) })
+  })
+}
+
+module.exports.getLinkUrl = getLinkUrlFromUser
+
+/**
+ * @typedef {Object} Links
+ * @prop {number} index The reference number used for the inline link
+ * @prop {string} url The url the link points to
+ * @prop {number} lineNum The line number the link resides on
+ */
