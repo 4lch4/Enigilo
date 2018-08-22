f9ba0b1042d85dd7b4f688fe89fc80c792335f8b
Devin Leaman <dleaman212@gmail.com>
2018-08-22 06:10:27 -0500

    Added the InsertImage command

---
+++ cmds/InsertImage.js
@@ -0,0 +1,27 @@
+const lTools = require('../tools/LinkTools')
+const eTools = require('../tools/EditorTools')
+
+const vscode = require('vscode')
+const Window = vscode.window
+
+const handleEmptySelection = () => { return false }
+
+module.exports = () => {
+  try {
+    let editor = Window.activeTextEditor
+    let selection = editor.selection
+    if (selection.isEmpty) handleEmptySelection()
+
+    lTools.getLinkUrl(Window).then(url => {
+      if (url === undefined || url.length === 0) return
+
+      lTools.getNewReference(url, editor.document).then(newLink => {
+        eTools.insertImageReferenceText(selection, newLink).then(res => {
+          if (!newLink.existed) eTools.insertReferenceToFile(newLink)
+        }).catch(err => Window.showErrorMessage(err.message))
+      }).catch(err => Window.showErrorMessage(err.message))
+    }).catch(err => Window.showErrorMessage(err.message))
+  } catch (error) {
+    console.log(error)
+  }
+}
--- extension.js
+++ extension.js
@@ -4,6 +4,7 @@ exports.activate = () => {
   console.log(`Enmeti has been activated.`)
 
   vscode.commands.registerCommand('enmeti.insertLink', require('./cmds/InsertLink'))
+  vscode.commands.registerCommand('enmeti.insertImage', require('./cmds/InsertImage'))
 }
 
 exports.deactivate = () => {}
