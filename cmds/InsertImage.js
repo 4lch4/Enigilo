b20841ff24bfe3a46dedcf4033cee997a2e39a60
Devin Leaman <dleaman212@gmail.com>
2018-10-27 00:08:23 -0500

    Added changes to InsertImage
    Now it uses the same methods as insertLink.

--- cmds/InsertImage.js
+++ cmds/InsertImage.js
@@ -1,10 +1,29 @@
+const { getConfigProperty, showMessage } = require('../tools/StdTools')
+const props = require('../tools/Properties')
 const lTools = require('../tools/LinkTools')
 const eTools = require('../tools/EditorTools')
 
 const vscode = require('vscode')
 const Window = vscode.window
 
-const handleEmptySelection = () => { return false }
+const handleEmptySelection = async () => {
+  if (getConfigProperty(props.handleEmptySelection)) {
+    let editor = Window.activeTextEditor
+
+    let url = await lTools.getLinkUrlFromUser('What is the URL/path of the image you wish to display?')
+    let linkText = await lTools.getLinkTextFromUser()
+    let newRef = await lTools.getNewReference(url, editor.document)
+
+    let edited = await editor.edit(builder => {
+      let newText = `[${linkText}][${newRef.index}]`
+
+      builder.insert(editor.selection.start, newText)
+    })
+
+    if (edited && !newRef.existed) eTools.insertReferenceToFile(newRef)
+    else if (!edited) showMessage(Window, 'The text could not be edited successfully, please try again.')
+  }
+}
 
 /**
  * The function/command for inserting an image into a Markdown file.
@@ -13,17 +32,17 @@ const insertImage = async () => {
   try {
     let editor = Window.activeTextEditor
     let selection = editor.selection
-    if (selection.isEmpty) handleEmptySelection()
+    if (selection.isEmpty) return handleEmptySelection()
 
     let url = await lTools.getLinkUrlFromUser('What is the URL/path of the image you wish to display?')
-    if (lTools.checkUrl(url)) {
-      // Verify the link doesn't exist as a separate reference. If it does, retrieve it.
-      let newRef = await lTools.getNewReference(url, editor.document)
-      let edited = await eTools.insertImageReferenceText(selection, newRef)
 
-      // If the edit succeeded and the link didn't already exist, add it to the file
-      if (edited && !newRef.existed) eTools.insertReferenceToFile(newRef)
-    }
+    // Verify the link doesn't exist as a separate reference. If it does, retrieve it.
+    let newRef = await lTools.getNewReference(url, editor.document)
+    let edited = await eTools.insertImageReferenceText(selection, newRef)
+
+    // If the edit succeeded and the link didn't already exist, add it to the file
+    if (edited && !newRef.existed) eTools.insertReferenceToFile(newRef)
+    else if (!edited) showMessage(Window, 'The selected text could not be edited successfully, please try again.')
   } catch (err) { console.error(err) }
 }
 
